50cffb9f823f9d6f8093e21fd6cb7e5efa585966
nhphucqt <hoangphuc.qti@gmail.com>
2024-01-27 19:36:30 +0700

    update

--- tools/test_city_person.py
+++ tools/test_city_person.py
@@ -36,7 +36,7 @@ def single_gpu_test(model, data_loader, show=False, save_img=False, save_img_dir
         print("rawimage:", data["raw_img"].shape)
         print("image:", type(data["img"]))
         print("image:", data["img"][0].shape)
-        raw_img = T.ToPILImage()(data.pop("raw_img"))
+        raw_img = T.ToPILImage()(data.pop("raw_img")[0])
         raw_img.save("/content/img"+str(i)+".jpg")
         with torch.no_grad():
             result = model(return_loss=False, rescale=not show, **data)
