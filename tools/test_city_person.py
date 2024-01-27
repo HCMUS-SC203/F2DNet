83c0a3630aaf6b5debbfcc6756f237f07bb17780
nhphucqt <hoangphuc.qti@gmail.com>
2024-01-27 16:46:32 +0700

    update

--- tools/test_city_person.py
+++ tools/test_city_person.py
@@ -37,7 +37,7 @@ def single_gpu_test(model, data_loader, show=False, save_img=False, save_img_dir
         print(type(data["img"]))
         print(len(data["img"]))
         print(type(data["img"][0]))
-        print(len(data["img"][0]))
+        print(data["img"][0].size(0))
         prob, result = filterclip(data, result)
         results.append(result)
 
