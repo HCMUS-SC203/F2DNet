8341cd294fdb676c240265931f6ac42f01b78b6c
ldn694 <70439334+ldn694@users.noreply.github.com>
2024-02-22 00:37:47 +0700

    Added labelsVisualizer.py

--- labelsVisualizer.py
+++ labelsVisualizer.py
@@ -63,7 +63,7 @@ def visualize_labels():
         # change img to jpg
         # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         for obj in gt['objects']:
-            x1, y1, x2, y2 = obj['bbox']
+            x1, y1, width, height = obj['bbox']
             # vis_x1, vis_y1, vis_x2, vis_y2 = obj['bboxVis']
             label = obj['label']
             label_set.add(label)
