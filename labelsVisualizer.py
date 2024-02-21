f38246cf94c43292805d4cbfe8d0b6441c0e6212
ldn694 <70439334+ldn694@users.noreply.github.com>
2024-02-21 23:03:15 +0700

    Added labelsVisualizer.py

--- labelsVisualizer.py
+++ labelsVisualizer.py
@@ -52,7 +52,7 @@ def visualize_labels():
         print(gt_path)
         with open(gt_path, 'r') as f:
             gt = json.load(f)
-        img = cv2.imread(img_path)
+        # img = cv2.imread(img_path)
         for obj in gt['objects']:
             # x1, y1, x2, y2 = obj['bbox']
             # vis_x1, vis_y1, vis_x2, vis_y2 = obj['bboxVis']
